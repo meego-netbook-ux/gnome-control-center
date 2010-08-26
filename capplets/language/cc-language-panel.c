2c60d0be6525dbd524981d3f1e2fff41cc5ac464
Thomas Wood <thomas.wood@intel.com>
2010-08-26 14:54:07 +0100

    Add Language settings panel

---
+++ capplets/language/cc-language-panel.c
@@ -0,0 +1,143 @@
+/*
+ * language-panels
+ * Copyright (C) 2010 Intel Corporation
+ * 
+ * language-panels is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * language-panels is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "cc-language-panel.h"
+
+G_DEFINE_DYNAMIC_TYPE (CcLanguagePanel, cc_language_panel, CC_TYPE_PANEL);
+
+static void
+map (GtkWidget       *socket)
+{
+  gchar *command;
+  GError *err = NULL;
+  static GdkNativeWindow win = 0;
+  GdkNativeWindow new_win;
+  
+  g_debug ("map");
+
+  new_win = gtk_socket_get_id (GTK_SOCKET (socket));
+
+  /* map is called multiple times */
+  if (new_win == win)
+    return;
+  win = new_win;
+
+  command = g_strdup_printf ("system-config-language --socket %d", win);
+
+
+  g_debug ("Running %s", command);
+  g_spawn_command_line_async (command, &err);
+
+  if (err)
+    {
+      g_warning ("Error loading system-config-language: %s", err->message);
+      g_error_free (err);
+    }
+}
+
+static gboolean
+plug_removed (GtkSocket *socket,
+              CcPanel   *panel)
+{
+  CcShell *shell;
+
+  shell = cc_panel_get_shell (panel);
+
+  cc_shell_set_panel (shell, NULL);
+
+  return FALSE;
+}
+
+static void
+active_changed_cb (CcLanguagePanel *panel,
+                   gboolean is_active,
+                   gpointer *data)
+{
+  if (is_active)
+    {
+      panel->socket = gtk_socket_new ();
+      gtk_container_add (GTK_CONTAINER (panel), panel->socket);
+
+      g_signal_connect (panel->socket, "map", G_CALLBACK (map), NULL);
+      g_signal_connect (panel->socket, "plug-removed",
+                        G_CALLBACK (plug_removed), panel);
+      gtk_widget_show (panel->socket);
+    }
+  else
+    {
+      gtk_widget_destroy (panel->socket);
+      panel->socket = NULL;
+    }
+
+}
+
+static void
+cc_language_panel_init (CcLanguagePanel *object)
+{
+  g_signal_connect (object, "active-changed", G_CALLBACK (active_changed_cb),
+                    NULL);
+  object->socket = NULL;
+}
+
+static void
+cc_language_panel_finalize (GObject *object)
+{
+  G_OBJECT_CLASS (cc_language_panel_parent_class)->finalize (object);
+}
+
+static GObject*
+cc_language_panel_constructor (GType                  type,
+                               guint                  n_construct_properties,
+                               GObjectConstructParam *construct_properties)
+{
+  CcLanguagePanel *language_panel;
+
+  language_panel = CC_LANGUAGE_PANEL (G_OBJECT_CLASS (cc_language_panel_parent_class)->constructor (type,
+                                                                                                  n_construct_properties,
+                                                                                                  construct_properties));
+  g_object_set (language_panel,
+                "display-name", ("Language"),
+                "id","system-config-language.desktop",
+                NULL);
+
+  return G_OBJECT (language_panel);
+}
+
+static void
+cc_language_panel_class_init (CcLanguagePanelClass *klass)
+{
+  GObjectClass* object_class = G_OBJECT_CLASS (klass);
+
+  object_class->finalize = cc_language_panel_finalize;
+  object_class->constructor = cc_language_panel_constructor;
+}
+
+static void
+cc_language_panel_class_finalize (CcLanguagePanelClass *klass)
+{
+}
+
+void
+cc_language_panel_register (GIOModule *module)
+{
+  cc_language_panel_register_type (G_TYPE_MODULE (module));
+  g_io_extension_point_implement (CC_PANEL_EXTENSION_POINT_NAME,
+                                  CC_TYPE_LANGUAGE_PANEL,
+                                  "language",
+                                  10);
+}
